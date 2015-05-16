57832cf0f34d75bd142b31858fa33fddce03f9e8
Frank Rue <frue@bocodigital.com>
2015-05-16 14:11:44 -0400

    finished media queries and swipe

--- js/app/app.js
+++ js/app/app.js
@@ -16,6 +16,9 @@ require([
 
   'use strict';
 
+  var $window;
+  var $header;
+
   // early start for the fonts
   TypeKitFonts.init(document);
 
@@ -31,6 +34,12 @@ require([
   function init() {
 
     FastClick.attach(document.body);
+    $window = $(window);
+    $header = $('header');
+    $('body').swipe({
+      threshold: 75,
+      swipe: onSwipe
+    });
 
   }
 
@@ -38,7 +47,11 @@ require([
   function bind() {
 
     $('header nav a').click(onNavClick);
+    $('header nav li:first a').click();
     $('footer .country select').selectize();
+    $('header button').click(onNavToggle);
+    $(window).scroll(onScroll);
+    $(window).resize(onResize);
 
   }
 
@@ -53,6 +66,45 @@ require([
       left: newLeft,
       ease: Back.easeOut
     });
+
+  }
+
+  function onNavToggle(e) {
+    e.preventDefault();
+    $('header nav').toggleClass('open');
+    $(this).toggleClass('on');
+  }
+
+  function onScroll() {
+    if ($window.scrollTop() > 20) {
+      $header.addClass('affix');
+    } else {
+      $header.removeClass('affix');
+    }
+  }
+
+  function onResize() {
+    $('header nav li:first a').click();
+  }
+
+  function onSwipe( event, direction ) {
+
+    switch(direction) {
+
+      case 'left':
+      $('header nav').addClass('open');
+      $('header button').addClass('on');
+      break;
+
+      case 'right':
+      $('header nav').removeClass('open');
+      $('header button').removeClass('on');
+      break;
+
+      default:
+      break;
+
+    }
 
   }
 
